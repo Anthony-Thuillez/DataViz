9b035d0bf903be389f79c4b5f1f6f758a466b297
Thuillez <idealistick@outlook.fr>
2019-04-11 18:42:58 +0200

    integration components

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,19 +1,108 @@
 import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
+import './reset.scss';
 import './style.scss';
 
-const Home = () =>
-    <div>
-        <h1>Home</h1>
-    </div>
+class Integration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleActiveClass = this.toggleActiveClass.bind(this);
+        this.state = {
+            isActive: false
+        }
+    }
+
+    toggleActiveClass() {
+        const currentState = this.state.isActive;
+        this.setState({ isActive: !currentState });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h1>Liste des composants :</h1>
+
+                <h2 className="title">Couleurs :</h2>
+                <div className="container">
+                    <div className="block blue-primery"></div>
+                    <div className="block blue-secondary"></div>
+                    <div className="block gold"></div>
+                </div>
+
+                <h2 className="title">Bulles champions :</h2>
+                <div className="container">
+                    <div className="thumb-champ"></div>
+                    <div className="thumb-champ"></div>
+                    <div className="thumb-champ"></div>
+                    <div className="thumb-champ"></div>
+                    <div className="thumb-champ"></div>
+                </div>
+
+                <h2 className="title">Bulles des postes :</h2>
+                <div className="container">
+                    <div className="thumb-post">
+                        <p>Top</p>
+                    </div>
+                    <div className="thumb-post">
+                        <p>Jungle</p>
+                    </div>
+                    <div className="thumb-post">
+                        <p>Mid</p>
+                    </div>
+                    <div className="thumb-post">
+                        <p>Bot</p>
+                    </div>
+                    <div className="thumb-post">
+                        <p>Support</p>
+                    </div>
+                </div>
+
+                <h2 className="title">Bouttons Ranking :</h2>
+                <div className="container space-around">
+                    <div 
+                        className={this.state.isActive ? 'btn-rank active' : 'btn-rank'}
+                        onClick={this.toggleActiveClass}
+                    >
+                        <span>S</span>
+                    </div>
+                    <div
+                        className={this.state.isActive ? 'btn-rank active' : 'btn-rank'}
+                        onClick={this.toggleActiveClass}
+                    >
+                        <span>A</span>
+                    </div>
+                    <div
+                        className={this.state.isActive ? 'btn-rank active' : 'btn-rank'}
+                        onClick={this.toggleActiveClass}
+                    >
+                        <span>B</span>
+                    </div>
+                    <div
+                        className={this.state.isActive ? 'btn-rank active' : 'btn-rank'}
+                        onClick={this.toggleActiveClass}
+                    >
+                        <span>C</span>
+                    </div>
+                    <div
+                        className={this.state.isActive ? 'btn-rank active' : 'btn-rank'}
+                        onClick={this.toggleActiveClass}
+                    >
+                        <span>D</span>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
 
 class App extends Component {
     render() {
         return(
             <Router>
                 <Fragment>
-                    <Route exact path="/" component={Home}/> {/* root page */}
+                    <Route exact path="/" component={Integration}/> {/* root page */}
                 </Fragment>
             </Router>
         )
