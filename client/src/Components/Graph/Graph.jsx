f8c08fcc38c64657db27a54ca4712c52927a68cf
Mehdi <mehdi.verfaillie@hetic.net>
2019-04-13 13:50:37 +0200

    additional funcs + comments + unit test

--- client/src/Components/Graph/Graph.jsx
+++ client/src/Components/Graph/Graph.jsx
@@ -1,7 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import SortByRate from './SortByRate'
-class Graph extends Component {
-    
+import data from '../../data.json'
+class Graph extends Component { 
+    componentDidMount() {
+        SortByRate.getAllChampions(data)
+        SortByRate.getPickRateAllChampions(data)
+        SortByRate.orderByPickRate(data)
+        SortByRate.medianPickRate(data)
+
+        SortByRate.getWinRateAllChampions(data)
+        SortByRate.orderByWinRate(data)
+        SortByRate.medianWinRate(data)
+
+        SortByRate.getBanRateAllChampions(data)
+        SortByRate.orderByBanRate(data)
+        SortByRate.medianBanRate(data)
+    }
     render() {
         return(
             <div></div>
--- client/src/Components/Graph/SortByRate.js
+++ client/src/Components/Graph/SortByRate.js
@@ -1,10 +1,17 @@
-const getAllChampions = (data_mooc) => {
-    return data_mooc.length
+const getAllChampions = (data) => {
+    return data.length
 }
 
-const getpickRateAllChampions = (data_mooc) => {
+/**
+ * Functions purpose : Sort By Pick Rate
+ * @param data come from data.json
+ * @param new_array @return an array of ordered pick rate
+ * @param medianPickRate() @return the median of @param new_array
+ */
+
+const getPickRateAllChampions = (data) => {
     let pickArr = []
-    let pickRate = data_mooc.map(e => ({
+    let pickRate = data.map(e => ({
         pick: e.pick
     }))
     
@@ -15,8 +22,8 @@ const getpickRateAllChampions = (data_mooc) => {
     return pickArr
 }
 
-const orderByPickRate = (data_mooc) => {
-    let new_array = getpickRateAllChampions(data_mooc)
+const orderByPickRate = (data) => {
+    let new_array = getPickRateAllChampions(data)
     new_array = new_array.map(Number)
     for (let i = 0; i < new_array.length; i++) {
         // check if array value is false or NaN
@@ -30,8 +37,8 @@ const orderByPickRate = (data_mooc) => {
     return new_array
 }
 
-const median = (data_mooc) => {
-    let array = orderByPickRate(data_mooc)
+const medianPickRate = (data) => {
+    let array = orderByPickRate(data)
     if (array.length === 0) return 0;
 
     array.sort(function (a, b) {
@@ -42,12 +49,122 @@ const median = (data_mooc) => {
 
     if (array.length % 2)
         return array[half];
-    
+
     return (array[half - 1] + array[half]) / 2.0;
+}
+
+/**
+ * Functions purpose : Sort By Win Rate
+ * @param data come from data.json
+ * @param new_array @return an array of ordered Win rate
+ * @param medianWinRate() @return the median of @param new_array
+ */
+
+const getWinRateAllChampions = (data) => {
+    let winArr = []
+    let winRate = data.map(e => ({
+        pick: e.win
+    }))
+
+    winRate.forEach(function (element) {
+        let winVal = Object.values(element).pop()
+        winArr.push(winVal)
+    });
+    return winArr
+}
+
+const orderByWinRate = (data) => {
+    let new_array = getWinRateAllChampions(data)
+    new_array = new_array.map(Number)
+    for (let i = 0; i < new_array.length; i++) {
+        // check if array value is false or NaN
+        if (new_array[i] === false || Number.isNaN(new_array[i])) {
+            new_array.splice(i, 1)
+        }
+    }
+    new_array.sort(function orderRates(a, b) {
+        return b - a;
+    })
+    return new_array
+}
+
+const medianWinRate = (data) => {
+    let array = orderByWinRate(data)
+    if (array.length === 0) return 0;
+
+    array.sort(function (a, b) {
+        return a - b;
+    });
+
+    var half = Math.floor(array.length / 2);
+
+    if (array.length % 2)
+        return array[half];
+
+    return (array[half - 1] + array[half]) / 2.0;
+}
+
+/**
+ * Functions purpose : Sort By Ban Rate
+ * @param data come from data.json
+ * @param new_array @return an array of ordered Ban rate
+ * @param medianBanRate() @return the median of @param new_array
+ */
+
+const getBanRateAllChampions = (data) => {
+     let banArr = []
+     let banRate = data.map(e => ({
+         pick: e.ban
+     }))
+
+     banRate.forEach(function (element) {
+         let banVal = Object.values(element).pop()
+         banArr.push(banVal)
+     });
+     return banArr
+}
+
+const orderByBanRate = (data) => {
+     let new_array = getBanRateAllChampions(data)
+     new_array = new_array.map(Number)
+     for (let i = 0; i < new_array.length; i++) {
+         // check if array value is false or NaN
+         if (new_array[i] === false || Number.isNaN(new_array[i])) {
+             new_array.splice(i, 1)
+         }
+     }
+     new_array.sort(function orderRates(a, b) {
+         return b - a;
+     })
+     return new_array
+}
+
+const medianBanRate = (data) => {
+     let array = orderByBanRate(data)
+     if (array.length === 0) return 0;
+
+     array.sort(function (a, b) {
+         return a - b;
+     });
+
+     var half = Math.floor(array.length / 2);
+
+     if (array.length % 2)
+         return array[half];
+
+     return (array[half - 1] + array[half]) / 2.0;
 }
 module.exports = {
     getAllChampions,
-    getpickRateAllChampions,
+    getPickRateAllChampions,
     orderByPickRate,
-    median
+    medianPickRate,
+
+    getWinRateAllChampions,
+    orderByWinRate,
+    medianWinRate,
+
+    getBanRateAllChampions,
+    orderByBanRate,
+    medianBanRate
 }
