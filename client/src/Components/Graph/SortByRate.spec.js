14814f686892d4da4400315f1cc72387679e9425
Mehdi <mehdi.verfaillie@hetic.net>
2019-04-13 13:50:37 +0200

    additional funcs + comments + unit test

--- client/src/Components/Graph/SortByRate.spec.js
+++ client/src/Components/Graph/SortByRate.spec.js
@@ -1,17 +1,13 @@
 const SBR = require('./SortByRate')
 
-describe("Sort By Win/Lose/Pick Rate", function() {
-    const data_mooc = [{
+describe("Sort By Pick Rate", function() {
+    let data_mooc = [{
             "name": "Vel'koz",
-            "pick": "50",
-            "ban": "20",
-            "win": "60",
+            "pick": "50"
         },
         {
             "name": "Yasuo",
-            "pick": "80",
-            "ban": "80",
-            "win": "30",
+            "pick": "80"
         }
     ]
     it("1/ Get every champions", function () {
@@ -21,7 +17,7 @@ describe("Sort By Win/Lose/Pick Rate", function() {
 
     it("2/ Get pick rate of each champion", function() {
         let expected = ["50", "80"]
-        expect(SBR.getpickRateAllChampions(data_mooc)).toEqual(
+        expect(SBR.getPickRateAllChampions(data_mooc)).toEqual(
             expect.arrayContaining(expected),
         )
     })
@@ -35,6 +31,66 @@ describe("Sort By Win/Lose/Pick Rate", function() {
 
     it("4/ Calculate the median of the pick rates", function() {
         let expected = 65
-        expect(SBR.median(data_mooc)).toEqual(expected)
+        expect(SBR.medianPickRate(data_mooc)).toEqual(expected)
+    })
+})
+
+describe("Sort By Win Rate", function () {
+    let data_mooc = [{
+            "name": "Vel'koz",
+            "win": "60"
+        },
+        {
+            "name": "Yasuo",
+            "win": "30"
+        }
+    ]
+    it("1/ Get win rate of each champion", function () {
+        let expected = ["60", "30"]
+        expect(SBR.getWinRateAllChampions(data_mooc)).toEqual(
+            expect.arrayContaining(expected),
+        )
+    })
+
+    it("2/ Order champions by win rate", function () {
+        let expected = [60, 30]
+        expect(SBR.orderByWinRate(data_mooc)).toEqual(
+            expect.arrayContaining(expected),
+        )
+    })
+
+    it("3/ Calculate the median of the win rates", function () {
+        let expected = 45
+        expect(SBR.medianWinRate(data_mooc)).toEqual(expected)
+    })
+})
+
+describe("Sort By Ban Rate", function () {
+    let data_mooc = [{
+            "name": "Vel'koz",
+            "ban": "20"
+        },
+        {
+            "name": "Yasuo",
+            "ban": "80"
+        }
+    ]
+    it("1/ Get ban rate of each champion", function () {
+        let expected = ["20", "80"]
+        expect(SBR.getBanRateAllChampions(data_mooc)).toEqual(
+            expect.arrayContaining(expected),
+        )
+    })
+
+    it("2/ Order champions by ban rate", function () {
+        let expected = [80, 20]
+        expect(SBR.orderByBanRate(data_mooc)).toEqual(
+            expect.arrayContaining(expected),
+        )
+    })
+
+    it("3/ Calculate the median of the ban rates", function () {
+        let expected = 50
+        expect(SBR.medianBanRate(data_mooc)).toEqual(expected)
     })
 })
