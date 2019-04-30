436e3ad5dec1929ef952ba4dc172e5eb870e8c70
Mehdi <mehdi.verfaillie@hetic.net>
2019-04-30 09:44:05 +0200

    fix func paramater

--- client/src/Components/Median/Median.jsx
+++ client/src/Components/Median/Median.jsx
@@ -5,15 +5,14 @@ import data from '../../data.json'
 class Median extends Component {
 
     componentDidMount() {
-        SortByRate.getChampByPost(data, "pick")
-        SortByRate.medianRate(data, "pick")
+        SortByRate.medianRate(data, "pick", "top")
     }
     render() {
         return(
             <Fragment>
-            <div>PickRate median : {SortByRate.medianRate(data, "pick")}%</div>
-            <div>WinRate median : {SortByRate.medianRate(data, "win")}%</div>
-            <div>BanRate median : {SortByRate.medianRate(data, "ban")}%</div>
+            {/* <div>PickRate median : {SortByRate.medianRate(data, "pick", "mid")}%</div>
+            <div>WinRate median : {SortByRate.medianRate(data, "win", "mid")}%</div>
+            <div>BanRate median : {SortByRate.medianRate(data, "ban", "mid")}%</div> */}
             </Fragment>
         )
     }
