45099826f8b28bf6eefee614ca771904faa6ab64
Thuillez <idealistick@outlook.fr>
2019-05-02 19:31:54 +0200

    resolve conflict

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -4,11 +4,10 @@ import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Intro from './views/intro';
 import Landing from './views/landing';
 import Fiche from './views/fiche';
-import Median from './Median';
-import Integration from './views/integration'
-import ChartStats from './components/ChartStats/ChartStats'
-import Chart from './views/graph/Chart'
-import SearchBar from './views/SearchBar/SearchBar'
+import ChartStats from './components/ChartStats/ChartStats';
+import Chart from './views/graph/Chart';
+import SearchBar from './views/SearchBar/SearchBar';
+import Integration from './views/integration';
 
 import './styles/main.scss';
 
@@ -19,12 +18,11 @@ class App extends Component {
                 <>
                     <Route exact path="/" component={Intro} /> {/* root page */}
                     <Route path="/Landing" component={Landing} />
-                    <Route path="/Chart" component={Chart} />
                     <Route path="/Fiche" component={Fiche} />
-                    <Route path="/Median" component={Median} />
-                    <Route path="/Integration" component={Integration} />
                     <Route path="/ChartStats" component={ChartStats} />
+                    <Route path="/Chart" component={Chart} />
                     <Route path="/SearchBar" component={SearchBar} />
+                    <Route path="/Integration" component={Integration} />
                 </>
             </Router>
         )
