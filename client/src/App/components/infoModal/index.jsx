e086ccf8cf295c36da72d9f78bc3dd1a271afeda
Thuillez <idealistick@outlook.fr>
2019-05-03 18:19:07 +0200

    modal js

--- client/src/App/components/infoModal/index.jsx
+++ client/src/App/components/infoModal/index.jsx
@@ -6,13 +6,26 @@ import ImgGraph from '../../assets/img/Hiw.png';
 import ImgRank from '../../assets/img/Hiw-rank.png';
 
 class Modal extends Component {
+    state = {
+        active: false,
+
+    }
+
+    isActive = () => {
+        this.setState({ active: true });
+    }
+
+    isNotActive = () => {
+        this.setState({ active: false });
+    }
 
     render() {
+
         return (
             <>
-                <div className="btn-info"></div>
-                <div className="modal-info">
-                    <div className="icon icon-cross"></div>
+                <div onClick={this.isActive} className="btn-info"></div>
+                <div className={this.state.active ? 'modal-info active' : 'modal-info'}>
+                    <div onClick={this.isNotActive} className="icon icon-cross"></div>
                     <div className="modal-content">
                         <h2 className="title">How it works ?</h2>
                         <div className="img-graph">
@@ -22,12 +35,11 @@ class Modal extends Component {
                             <p>The size of the rounds content the images corresponds to the tier rank.</p>
                             <img className="img-rank" src={ImgRank} alt="rang" />
                         </div>
-                        <div className="btn-close">Keep going</div>
+                        <div onClick={this.isNotActive} className="btn-close">Keep going</div>
                     </div>
                 </div>
             </>
         )
     }
 }
-
 export default Modal
