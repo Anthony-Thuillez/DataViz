5b463612062047a44112d83eafd1aeec163a54e1
Mehdi <mehdi.verfaillie@hetic.net>
2019-05-23 23:00:49 +0200

    selecting champions for compare /done

--- client/src/App/views/compare/index.jsx
+++ client/src/App/views/compare/index.jsx
@@ -4,35 +4,66 @@ import data from '../../../data.json'
 
 class Compare extends Component {
     state =  {
-        champions: []
+        champions: [],
+        slot_left_name: '',
+        slot_right_name: ''
     }
     getDataRole = (event) => {
         let role = event.target.getAttribute('datarole')
         let champions = SortByRate.getChampByRole(data, role)
         this.setState({
             champions: champions
-        })
-
+        })        
     }
     
     retrieveChampCaracteristics(e) {
         let championName = e.target.textContent
         let champCaracteristics = SortByRate.getChampByName(data, championName)
-
+        console.log(champCaracteristics);
         let name_slotLeft = document.querySelector('.champion-slot-left-name')
         let name_slotRight = document.querySelector('.champion-slot-right-name')
 
-        if (name_slotLeft.innerHTML === '' && name_slotRight.innerHTML === '') {
-            name_slotLeft.innerHTML = champCaracteristics.name
-
+        if (this.state.slot_left_name === champCaracteristics.name) {
+            name_slotLeft.innerHTML = ''
+            this.setState({
+                slot_left_name: ''
+            })
         }
+        if (this.state.slot_right_name === champCaracteristics.name) {
+            name_slotRight.innerHTML = ''
+            this.setState({
+                slot_right_name: ''
+            })
+        }
+        
+        if (this.state.slot_left_name === '' && this.state.slot_right_name !== champCaracteristics.name) {
+            this.setState({
+                slot_left_name: champCaracteristics.name
+            })
+            name_slotLeft.innerHTML = champCaracteristics.name
+        }  
+        if (this.state.slot_right_name === '' && this.state.slot_left_name !== champCaracteristics.name) {
+            if (this.state.slot_left_name !== '' || this.state.slot_right_name !== '') {
+                this.setState({
+                    slot_right_name: champCaracteristics.name
+                })
+                name_slotRight.innerHTML = champCaracteristics.name
+            }
+        }
+        
+
+        // if (name_slotLeft.innerHTML === '' && name_slotRight.innerHTML === '') {
+        //     name_slotLeft.innerHTML = champCaracteristics.name
+
+        // }
     }
-        //STEP 1 => Au click, récupérer les infos d'un champion en fonction de son nom (name, img, win, pick, ban) DONE
+    //DONE => Au click, récupérer les infos d'un champion en fonction de son nom (name, img, win, pick, ban) 
     //STEP 2 => Au click, positionner le (nom, img) du champion cliqué dans champion-slot-right si champion-slot-right [is empty]
     //STEP 3 => Au click sur un deuxème element, si champion-slot-right [is not empty] ajouter le deuxième élément cliqué dans champion-slot-left
     //SETP 4 => Si les 2 champion-slot [is not empty], grisé toutes les autres champions
 
-    render() {                
+    render() {  
+              
         return(
             <Fragment>
                 <div className="champion-slot-left">
