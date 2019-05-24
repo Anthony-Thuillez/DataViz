b3855b03f9f765ebfe4f44e23d488aa5f606e2e9
Simon <soleausimon@hotmail.fr>
2019-05-24 11:50:10 +0200

    pentagone aquaData

--- client/src/App/scripts/liquidChart.js
+++ client/src/App/scripts/liquidChart.js
@@ -16,10 +16,10 @@ class LiquidGauge extends React.Component {
         waveRise: true,
         waveHeightScaling: true,
         waveAnimate: true,
-        waveColor: "#007DBC",
+        waveColor: "#00CBE0",
         waveOffset: 0,
         textVertPosition: 0.5,
-        textSize: 1,
+        textSize: 0.8,
         valueCountUp: true,
         displayPercent: true,
         textColor: "#045681",
@@ -38,7 +38,7 @@ class LiquidGauge extends React.Component {
             .attr("transform", "translate(" + this.waveAnimateScale(1) + ",0)")
             .attr("T", 1)
             .on("end", (d, i, nodes) => {
-                d3.select(nodes[i]).attr( "T", 0 );
+                d3.select(nodes[i]).attr("T", 0);
                 this.animateWave(this.state.waveAnimateTime);
             });
     }
@@ -48,13 +48,32 @@ class LiquidGauge extends React.Component {
 
         var waveHeightScale;
         var gauge = d3.select("#fillgauge");
+        var width = parseInt(gauge.style("width"))
+        var height = parseInt(gauge.style("height"))
+        var centerX = width / 2;
+        var centerY = height / 2;
+        var numPoints = 6;
+        var strokePenta = 3;
         var radius =
             Math.min(
-                parseInt(gauge.style("width")),
-                parseInt(gauge.style("height"))
+                width,
+                height
             ) / 2;
-        var locationX = parseInt(gauge.style("width")) / 2 - radius;
-        var locationY = parseInt(gauge.style("height")) / 2 - radius;
+        const points = d3.range(numPoints)
+            .map(i => {
+                const angle = i / numPoints * Math.PI * 2 + Math.PI;
+                return {
+                    x: Math.sin(angle) * radius + centerX,
+                    y: Math.cos(angle) * radius + centerY
+                };
+            });
+        const wheelLines = d3.range(numPoints).map(i => ({
+            x1: points[i].x,
+            y1: points[i].y,
+            x2: points[(i + 1) % numPoints].x,
+            y2: points[(i + 1) % numPoints].y
+        }));
+        
         var fillPercent =
             Math.max(config.minValue, Math.min(config.maxValue, this.props.value)) /
             config.maxValue;
@@ -68,7 +87,6 @@ class LiquidGauge extends React.Component {
                 .range([config.waveHeight, config.waveHeight])
                 .domain([0, 100]);
         }
-
         var textPixels = config.textSize * radius / 2;
         var textFinalValue = parseFloat(this.props.value).toFixed(2);
         var textStartValue = config.valueCountUp ? config.minValue : textFinalValue;
@@ -78,12 +96,10 @@ class LiquidGauge extends React.Component {
         var fillCircleMargin = circleThickness + circleFillGap;
         var fillCircleRadius = radius - fillCircleMargin;
         var waveHeight = fillCircleRadius * waveHeightScale(fillPercent * 100);
-
         var waveLength = fillCircleRadius * 2 / config.waveCount;
         var waveClipCount = 1 + config.waveCount;
         var waveClipWidth = waveLength * waveClipCount;
 
-        // Rounding functions so that the correct number of decimal places is always displayed as the value counts up.
         var textRounder = function (value) {
             return Math.round(value);
         };
@@ -98,35 +114,25 @@ class LiquidGauge extends React.Component {
             };
         }
 
-        // Data for building the clip wave area.
         var data = [];
         for (var i = 0; i <= 40 * waveClipCount; i++) {
             data.push({ x: i / (40 * waveClipCount), y: i / 40 });
         }
 
-        // Scales for drawing the outer circle.
-        var gaugeCircleX = d3.scaleLinear().range([0, 2 * Math.PI]).domain([0, 1]);
-        var gaugeCircleY = d3.scaleLinear().range([0, radius]).domain([0, radius]);
-
-        // Scales for controlling the size of the clipping path.
         var waveScaleX = d3.scaleLinear().range([0, waveClipWidth]).domain([0, 1]);
         var waveScaleY = d3.scaleLinear().range([0, waveHeight]).domain([0, 1]);
 
-        // Scales for controlling the position of the clipping path.
         var waveRiseScale = d3.scaleLinear()
-            // The clipping area size is the height of the fill circle + the wave height, so we position the clip wave
-            // such that the it will overlap the fill circle at all when at 0%, and will totally cover the fill
-            // circle at 100%.
+
             .range([
                 fillCircleMargin + fillCircleRadius * 2 + waveHeight,
                 fillCircleMargin - waveHeight
             ])
             .domain([0, 1]);
         this.waveAnimateScale = d3.scaleLinear()
-            .range([0, waveClipWidth - fillCircleRadius * 2]) // Push the clip area one full wave then snap back.
+            .range([0, waveClipWidth - fillCircleRadius * 2])
             .domain([0, 1]);
 
-        // Scale for controlling the position of the text within the gauge.
         var textRiseScaleY = d3.scaleLinear()
             .range([
                 fillCircleMargin + fillCircleRadius * 2,
@@ -134,27 +140,10 @@ class LiquidGauge extends React.Component {
             ])
             .domain([0, 1]);
 
-        // Center the gauge within the parent SVG.
-        var gaugeGroup = gauge
-            .append("g")
-            .attr("transform", "translate(" + locationX + "," + locationY + ")");
+        var gaugeGroup = gauge.append("g")
+            .attr("transform", "translate(" + radius + "," + (centerY - radius) + ")");
 
-        // Draw the outer circle.
-        var gaugeCircleArc = d3
-            .arc()
-            .startAngle(gaugeCircleX(0))
-            .endAngle(gaugeCircleX(1))
-            .outerRadius(gaugeCircleY(radius))
-            .innerRadius(gaugeCircleY(radius - circleThickness));
-        gaugeGroup
-            .append("path")
-            .attr("d", gaugeCircleArc)
-            .style("fill", config.circleColor)
-            .attr("transform", "translate(" + radius + "," + radius + ")");
-
-        // Text where the wave does not overlap.
-        var text1 = gaugeGroup
-            .append("text")
+        var text1 = gaugeGroup.append("text")
             .text(textRounder(textStartValue) + percentText)
             .attr("class", "liquidFillGaugeText")
             .attr("text-anchor", "middle")
@@ -169,9 +158,7 @@ class LiquidGauge extends React.Component {
                 ")"
             );
 
-        // The clipping wave area.
-        var clipArea = d3
-            .area()
+        var clipArea = d3.area()
             .x(function (d) {
                 return waveScaleX(d.x);
             })
@@ -187,30 +174,33 @@ class LiquidGauge extends React.Component {
             .y1(function (d) {
                 return fillCircleRadius * 2 + waveHeight;
             });
-        var waveGroup = gaugeGroup
-            .append("defs")
+        var waveGroup = gaugeGroup.append("defs")
             .append("clipPath")
             .attr("id", "clipWave" + this.props.id);
-        this.wave = waveGroup
-            .append("path")
-            .datum(data)
+        this.wave = waveGroup.append("path")
+            .data([data])
             .attr("d", clipArea)
             .attr("T", 0);
 
-        // The inner circle with the clipping wave attached.
-        var fillCircleGroup = gaugeGroup
-            .append("g")
+        var fillCircleGroup = gaugeGroup.append("g")
             .attr("clip-path", "url(#clipWave" + this.props.id + ")");
-        fillCircleGroup
-            .append("circle")
-            .attr("cx", radius)
-            .attr("cy", radius)
-            .attr("r", fillCircleRadius)
+        fillCircleGroup.append("path")
+            .attr('d', `
+                M 0 0 
+                ${wheelLines.map(({ x2, y2 }) =>
+                'L ' + (x2 - 75) + " " + y2
+            )}`)
             .style("fill", config.waveColor);
+        gauge.selectAll('line').data(wheelLines)
+            .enter().append('line')
+            .attr('x1', d => d.x1)
+            .attr('y1', d => d.y1)
+            .attr('x2', d => d.x2)
+            .attr('y2', d => d.y2)
+            .attr('stroke', "#A6843C")
+            .attr('stroke-width', strokePenta);
 
-        // Text where the wave does overlap.
-        var text2 = fillCircleGroup
-            .append("text")
+        var text2 = fillCircleGroup.append("text")
             .text(textRounder(textStartValue) + percentText)
             .attr("class", "liquidFillGaugeText")
             .attr("text-anchor", "middle")
@@ -225,7 +215,6 @@ class LiquidGauge extends React.Component {
                 ")"
             );
 
-        // Make the value count up.
         if (config.valueCountUp) {
             var textTween = function () {
                 var i = d3.interpolate(this.textContent, textFinalValue);
@@ -237,16 +226,13 @@ class LiquidGauge extends React.Component {
             text2.transition().duration(config.waveRiseTime).tween("text", textTween);
         }
 
-        // Make the wave rise. wave and waveGroup are separate so that horizontal and vertical movement can be controlled independently.
         var waveGroupXPosition =
             fillCircleMargin + fillCircleRadius * 2 - waveClipWidth;
         if (config.waveRise) {
-            console.log(waveGroup)
-            waveGroup
-                .attr(
-                    "transform",
-                    "translate(" + waveGroupXPosition + "," + waveRiseScale(0) + ")"
-                )
+            waveGroup.attr(
+                "transform",
+                "translate(" + waveGroupXPosition + "," + waveRiseScale(0) + ")"
+            )
                 .transition()
                 .duration(config.waveRiseTime)
                 .attr(
