3658745a88e37f3babf87f3d5a1d9365df8973ff
Simon <soleausimon@hotmail.fr>
2019-06-14 10:46:15 +0200

    Logo intro text

--- client/src/views/logo/index.js
+++ client/src/views/logo/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Logo from '../../assets/LogoText.png';
 
 class LogoAnime extends React.Component {
     render() {
         return (
             <div class="loading">
                 <h1 class="logoAnime"> UL </h1>
+                <img src={Logo} alt="Logo" />
             </div>
         )
     }
