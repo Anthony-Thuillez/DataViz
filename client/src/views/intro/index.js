150bb44a8fdcaad7b79ac4a67c4dfc5fe74c7be1
Simon <soleausimon@hotmail.fr>
2019-06-14 10:20:43 +0200

    liquid logo

--- client/src/views/intro/index.js
+++ client/src/views/intro/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/LogoXL.png';
 import ImgLeft from '../../assets/img/HP-left.png';
 import ImgRight from '../../assets/img/HP-right.png';
+import LogoAnime from '../logo/index'
 
 class Intro extends Component {
 
@@ -16,7 +17,8 @@ class Intro extends Component {
                 <div className="block-intro">
                     <p>Hetic students are proud to present</p>
                     <div className="logo">
-                        <img src={Logo} alt="Logo" />
+                        <LogoAnime />
+                        {/* <img src={Logo} alt="Logo" /> */}
                     </div>
                     <p>A tool with which you can discover data on all League of Legend champions.</p>
                     <Link className="btn" to='/'>Launch data visualization</Link>
---
+++ client/src/views/logo/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class LogoAnime extends React.Component {
+    render() {
+        return (
+            <div class="loading">
+                <h1 class="logoAnime"> UL </h1>
+            </div>
+        )
+    }
+}
+
+export default LogoAnime
